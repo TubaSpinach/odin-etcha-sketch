ca4dd4a3e7f8398d1bf5d67429289262dddf4b62
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-10-27 21:46:47 -0400

    feat: add capability to change grid size

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-let divArray = []
+let divArray = [];
 let container = document.querySelector('#container');
+let enterBtn = document.querySelector('.enter');
+let dropdownBtn = document.querySelector('.gridnumber');
 
-for (let i=0; i<16;i++) {
-    divArray.push(document.createElement('div'));
-    
-    divArray[i].addEventListener('mouseenter', () => {
-        divArray[i].style.backgroundColor = "red"}
-    );
-    divArray[i].addEventListener('mouseleave', () => {
-        divArray[i].style.backgroundColor = "white"}
-    );
-    
-    container.appendChild(divArray[i]);
+
+
+
+makeGridBySides(4);
+function makeGridBySides(strOfSquares) {
+    clearGridSquares();
+    let numberOfSquares = parseInt(strOfSquares);
+
+    for (let i = 0; i<numberOfSquares*numberOfSquares; i++) {
+        divArray.push(document.createElement('div'));
+        
+        let gridPercentage = 100 / numberOfSquares
+        divArray[i].style.width = `${.9*gridPercentage}%`;
+        divArray[i].style.height = `${.9*gridPercentage}%`;
+
+        divArray[i].addEventListener('mouseenter', () => {
+            divArray[i].style.backgroundColor = "red"}
+        );
+        divArray[i].addEventListener('mouseleave', () => {
+            divArray[i].style.backgroundColor = "white"}
+        );
+        
+        
+        container.appendChild(divArray[i]);
+    }
 }
+
+function clearGridSquares() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+enterBtn.addEventListener('click',()=>{makeGridBySides(dropdownBtn.value)});
