1c7283cb44225c613ec8810b2ac445a72f95b84b
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-10-27 21:10:30 -0400

    feat: add hover color change

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ let container = document.querySelector('#container');
 
 for (let i=0; i<16;i++) {
     divArray.push(document.createElement('div'));
+    
+    divArray[i].addEventListener('mouseenter', () => {
+        divArray[i].style.backgroundColor = "red"}
+    );
+    divArray[i].addEventListener('mouseleave', () => {
+        divArray[i].style.backgroundColor = "white"}
+    );
+    
     container.appendChild(divArray[i]);
 }
