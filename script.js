5b335632daeb7608e408c843c03cf993cf931ca4
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2024-02-19 11:28:01 -0500

    feat: pad game state string with 0's

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ let container = document.querySelector('#container');
 let enterBtn = document.querySelector('.enter');
 let dropdownBtn = document.querySelector('.gridnumber');
 let clearBtn = document.querySelector('.clear');
+let grid_side_length = 4;
+let active_color = "red";
+let inactive_color = "white";
 
 
 
 
-makeGridBySides(4);
+makeGridBySides(grid_side_length);
 
 function makeGridBySides(strOfSquares) {
     clearGridSquares();
     let numberOfSquares = parseInt(strOfSquares);
+
+    //maybe this should be more explicit in the structure...
+    grid_side_length = numberOfSquares;
 
     for (let i = 0; i<numberOfSquares*numberOfSquares; i++) {
         divArray.push(document.createElement('div'));
@@ -24,11 +30,11 @@ function makeGridBySides(strOfSquares) {
         divArray[i].style.height = `${.9*gridPercentage}%`;
         */
         divArray[i].addEventListener('mouseenter', () => {
-            divArray[i].style.backgroundColor = "red"}
+            divArray[i].style.backgroundColor = active_color}
         );
         /*
         divArray[i].addEventListener('mouseleave', () => {
-            divArray[i].style.backgroundColor = "white"}
+            divArray[i].style.backgroundColor = active_color}
         );
         */
         
@@ -44,8 +50,9 @@ function clearGridSquares() {
 
 function resetSquareStyle() {
     let divArray = container.querySelectorAll('div');
-    divArray.forEach((div)=>{div.style.backgroundColor = "white"});
+    divArray.forEach((div)=>{div.style.backgroundColor = inactive_color});
 }
+
 function resetGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
@@ -56,12 +63,43 @@ function encodeGridStateAsString() {
     let grid_squares = document.querySelectorAll('#container div');
     let grid_state_str = '';
     grid_squares.forEach(function (square) {
-        grid_state_str += square.style.backgroundColor === "white" ? '0' : '1';        
+        grid_state_str += square.style.backgroundColor === inactive_color ? '0' : '1';        
     })
     return grid_state_str;
 }
 
+function decodeStringAsGridState(state_str) {
+    if(state_str.length !== grid_side_length*grid_side_length){
+        console.log(`string not long enough. Expected ${grid_side_length*grid_side_length} but got ${state_str.length}`)
+        return false;
+    } else {
+        let grid_squares = document.querySelectorAll('#container div');
+        grid_squares.forEach(function (square,currIndex) {
+            square.style.backgroundColor = state_str.charAt(currIndex) === '0' ? inactive_color : active_color;
+        });
+        return true;
+    }
+}
 
+function padGridStateString(state_str) {
+    let pad_row = ''
+    let padded_string = ''
+    for(let i = 0; i<=grid_side_length+1;i++){
+        pad_row += '0'
+
+        if(i<grid_side_length){
+            padded_string += '0' + state_str.slice(grid_side_length*i,grid_side_length*(i+1)) + '0';
+            console.log(`${padded_string}`)
+        }
+        
+    }
+    padded_string = pad_row + padded_string + pad_row
+    
+    return padded_string
+}
+function evolveGridState(state_str) {
+
+}
 
 clearBtn.addEventListener('click',()=>{resetSquareStyle()});
 enterBtn.addEventListener('click',()=>{makeGridBySides(dropdownBtn.value)});
